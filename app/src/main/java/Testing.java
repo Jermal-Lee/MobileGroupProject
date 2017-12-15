0b6d78ef46d313a8cdc78efc52acc7a6f2346888
jermal <long0225@algonquinlive.com>
2017-12-14 20:17:40 -0500

    Testing commits.

--- app/src/main/java/Testing.java
+++ app/src/main/java/Testing.java
@@ -1,6 +1,13 @@
+import android.util.Log;
+
 /**
  * Created by jerma on 2017-12-14.
  */
 
 public class Testing {
+
+    public void testing(){
+        Log.i("Testing", "Testing commits");
+    }
+
 }
